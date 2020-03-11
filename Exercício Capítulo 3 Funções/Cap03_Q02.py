303cd6d8ca60c468b4a09b8786748aca86414434
francisco-igor <61854132+francisco-igor@users.noreply.github.com>
2020-03-10 22:33:42 -0300

    Add files via upload

---
+++ Exercício Capítulo 3 Funções/Cap03_Q02.py
@@ -0,0 +1,15 @@
+def do_twice(o, v):
+    o(v)
+    o(v)
+
+def print_twice(v):
+    print(v)
+    print(v)
+
+def do_four(o, v):
+    do_twice(o, v)
+    do_twice(o,v)
+
+do_twice(print_twice, 'spam')
+print('--------------')
+do_four(print_twice, 'Hello, Python!')
